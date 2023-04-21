78bf791ec145fbc2de5e8b2ea71ac425ea566706
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2023-04-20 21:46:51 -0300

    feat: configuring colors and fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,37 @@
-import Home from './src'
+import React from 'react'
+
+import { ThemeProvider } from 'styled-components/native'
+import { Text } from 'react-native'
+
+import {
+  useFonts,
+  // eslint-disable-next-line camelcase
+  Poppins_400Regular,
+  // eslint-disable-next-line camelcase
+  Poppins_500Medium,
+  // eslint-disable-next-line camelcase
+  Poppins_600SemiBold,
+  // eslint-disable-next-line camelcase
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins'
+
+import Home from '@screens/Home'
+import theme from './src/global/theme'
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({
+    // eslint-disable-next-line camelcase
+    Poppins_400Regular,
+    // eslint-disable-next-line camelcase
+    Poppins_500Medium,
+    // eslint-disable-next-line camelcase
+    Poppins_600SemiBold,
+    // eslint-disable-next-line camelcase
+    Poppins_700Bold,
+  })
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Home /> : <Text>f</Text>}
+    </ThemeProvider>
+  )
 }
---
+++ src/global/theme.ts
@@ -0,0 +1,28 @@
+export default {
+  colors: {
+    primary: '#FFD600',
+    secondary: '#FF5531',
+    background: '#FCF9F2',
+    white_1: '#FCF9ED',
+    white: '#FFF',
+    black: '#1D1617',
+    black_100: '#7B6F72',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Poppins_400Regular',
+    MEDIUM: 'Poppins_500Medium',
+    SEMIBOLD: 'Poppins_600SemiBold',
+    BOLD: 'Poppins_700Bold',
+  },
+  FONT_SIZE: {
+    CAPTiON: 10,
+    SMALL: 12,
+    MEDIUM: 14,
+    LARGE: 16,
+    SUBTITLE: 18,
+    TITLE_H4: 20,
+    TITLE_H3: 22,
+    TITLE_H2: 24,
+    TITLE_H1: 26,
+  },
+}
